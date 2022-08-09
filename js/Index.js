5af0e2e51d313dbb1d0d7d777d26a9031348c32f
Arxiss <felipepv12321@gmail.com>
2022-08-09 07:38:46 -0500

    Update 3.0

---
+++ js/Index.js
@@ -0,0 +1,107 @@
+ScrollReveal().reveal('.triangles');
+ScrollReveal().reveal('.squares', { mobile: true });
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.scroll-titlle', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-10px'
+    });
+
+    sr.reveal('.tittle-ref', {
+        duration: 8000,
+        origin: 'top',
+        distance: '-20px'
+    });
+
+    sr.reveal('.btn-read', {
+        duration: 8000,
+        origin: 'top',
+        distance: '-30px'
+    });
+
+    sr.reveal('.about-scroll', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-500px'
+    });
+
+    sr.reveal('Aboutimg', {
+        duration: 11000,
+        origin: 'top',
+        distance: '-500px'
+    });
+
+    sr.reveal('.h4text', {
+        duration: 5000,
+        origin: 'left',
+        distance: '-120px'
+    });
+
+    sr.reveal('.h4p1', {
+        duration: 5000,
+        origin: 'left',
+        distance: '-120px'
+    });
+
+    sr.reveal('.h4p2', {
+        duration: 5000,
+        origin: 'left',
+        distance: '-120px'
+    });
+
+    sr.reveal('.portadalibro', {
+        duration: 5000,
+        origin: 'right',
+        distance: '-300px'
+    });
+
+    sr.reveal('.carousel-inner', {
+        duration: 5000,
+        origin: 'bottom',
+        distance: '-20px'
+    });
+
+    sr.reveal('.card1', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-60px'
+    });
+
+    sr.reveal('.card2', {
+        duration: 5000,
+        origin: 'top',
+        distance: '-60px'
+    });
+
+    sr.reveal('.card3', {
+        duration: 7000,
+        origin: 'top',
+        distance: '-60px'
+    });
+
+    sr.reveal('.instalogo', {
+        duration: 7000,
+        rotate: {
+            x: 5,
+            y: 180,
+        }
+    });
+
+    sr.reveal('.logowhitefin', {
+        duration: 10000,
+        origin: 'top',
+    });
+
+    sr.reveal('.finpag', {
+        duration: 10000,
+        origin: 'top',
+        distance: '-90px'
+    });
